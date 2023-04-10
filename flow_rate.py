1d9c811b25ea190f9aaf81625bccf90f9fcc9c4f
serpadina <115607872+serpadina@users.noreply.github.com>
2023-04-10 21:49:27 +0200

    Update flow_rate.py

--- flow_rate.py
+++ flow_rate.py
@@ -1,6 +1,5 @@
 hour = 2 #target of desired amount of time to run the experiment
 min = hour*60
-ml = 3
 flow_rates = [1, 2, 5, 10, 15, 20, 25, 30, 100] #examples of typical flow rates
 list_ul = []
 for i in flow_rates:
@@ -15,7 +14,7 @@ print(list_ml)
 print("With flow rates (im ul/min): ")
 print(flow_rates)
 
-for i in range(0, len(flow_rates)+1):
+for i in range(0, len(flow_rates)):
     flow_rate = flow_rates[i]
     volume = list_ml[i]
     print("For a flow rate of " + str(flow_rate) + " um/min, " + str(volume) + " ml of reagent solution will be used in " + str(hour) + " hours.")
